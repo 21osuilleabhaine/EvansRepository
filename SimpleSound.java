3e44126b0f43c507a3285b33b3ba34323d2c18a3
Evan O'Suilleabhain <21osuilleabhaine@cistercian.org>
2021-01-25 11:55:21 -0600

    Chords!
    Introduced a new constructor to add the capability for chords. Very excited about this.

--- SimpleSound.java
+++ SimpleSound.java
@@ -15,13 +15,19 @@ public class SimpleSound extends JFrame{
     Synthesizer syn;
     MidiChannel[] midChannel;
     Instrument[] instrument;
-    int xNote;
-    int yNote;
+    int Note1;
+    int Note2;
 
-    public void makeASound(int x, int y) {
-        xNote = x;
-        yNote = y;
-        this.midChannel[5].noteOn(x, y);
+    public void makeAChord(int x, int y) {
+        Note1 = x;
+        Note2 = y;
+        this.midChannel[5].noteOn(x, 550);
+        this.midChannel[5].noteOn(y, 550);
+        //The MIDI channel can be anything from 0 to 15. Anything below or above that will fail to produce a sound.
+    }
+    public void makeASound(int x) {
+        Note1 = x;
+        this.midChannel[5].noteOn(x, 550);
     }
     public SimpleSound(int x, int y) {
         this.setLayout(new BorderLayout());
@@ -43,7 +49,35 @@ public class SimpleSound extends JFrame{
 
             button1.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
-                    makeASound(x,y);
+                    makeAChord(x,y);
+                }
+            });
+
+        } catch (MidiUnavailableException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public SimpleSound(int x) {
+        this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        JButton button1 = new JButton("Try");
+        this.add(panel);
+        panel.add(button1);
+        this.pack();
+
+        try {
+            syn = MidiSystem.getSynthesizer();
+            syn.open();
+            midChannel = syn.getChannels();
+
+            instrument = syn.getDefaultSoundbank().getInstruments();
+            syn.loadInstrument(instrument[90]);
+
+            button1.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    makeASound(x);
                 }
             });
 
--- Tester.java
+++ Tester.java
@@ -2,7 +2,10 @@ public class Tester
 {
     public static void main(String[] args)
     {
-        SimpleSound a = new SimpleSound(55, 550);
+        SimpleSound a = new SimpleSound(55,70);
         a.setVisible(true);
     }
 }
+
+//It seems like the first variable controls the note/pitch, while the second variable controls volume.
+//It also seems like, if you want to make a chord, Note1 and Note2 should be 15 apart.
